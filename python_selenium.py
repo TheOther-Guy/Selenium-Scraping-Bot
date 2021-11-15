2f8fbf5bb6a7d3d369f7270c157c82c100bdf20c
kareem_722@hotmail.com <49558122+TheOther-Guy@users.noreply.github.com>
2021-11-15 18:30:29 +0200

    Update python_selenium.py

--- python_selenium.py
+++ python_selenium.py
@@ -2,4 +2,7 @@ import os
 from selenium import webdriver
 os.environ['PATH'] += r"C:/seleniumdrivers" # this slash should be forward slash else it won't work
 driver = webdriver.Chrome()
+driver.get('https://pixishoes.com/')
+shoes_category = driver.find_element_by_class_name("grid__item small--one-half medium-up--one-fifth")
+
 
