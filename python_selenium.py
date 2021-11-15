6cd1a34a30eb75d32ccbd25e822847f98ff8987f
kareem_722@hotmail.com <49558122+TheOther-Guy@users.noreply.github.com>
2021-11-15 19:50:41 +0200

    Update python_selenium.py

--- python_selenium.py
+++ python_selenium.py
@@ -1,8 +1,16 @@
 import os
 from selenium import webdriver
+import selenium
+import time
 os.environ['PATH'] += r"C:/seleniumdrivers" # this slash should be forward slash else it won't work
 driver = webdriver.Chrome()
 driver.get('https://pixishoes.com/')
-shoes_category = driver.find_element_by_class_name("grid__item small--one-half medium-up--one-fifth")
+
+closing_ad = driver.find_element_by_xpath('//*[@id="NewsletterPopup-newsletter-popup"]/div/div/button/svg')
+time.sleep(20)
+closing_ad.click()
+
+'''shoes_category = driver.find_element_by_class_name("grid__item small--one-half medium-up--one-fifth")
+shoes_category.click()'''
 
 
