68d1616c2d7171421185f3d81954806de67eb0b4
kareem_722@hotmail.com <49558122+TheOther-Guy@users.noreply.github.com>
2021-11-15 21:17:07 +0200

    .

--- python_selenium.py
+++ python_selenium.py
@@ -7,9 +7,10 @@ driver = webdriver.Chrome()
 driver.get('https://pixishoes.com/')
 
 time.sleep(10)
-closing_ad = driver.find_element_by_css_selector('#NewsletterPopup-newsletter-popup > div > div > button > svg > path')
-time.sleep(20)
-closing_ad.click()
+closing_ad = driver.find_element_by_css_selector('#shopify-section-16066484874a6d6f11 > div > div.grid.grid--uniform > div:nth-child(1)').click()
+
+'''time.sleep(20)
+closing_ad.click()'''
 
 '''shoes_category = driver.find_element_by_class_name("grid__item small--one-half medium-up--one-fifth")
 shoes_category.click()'''
