e0df1bf91340b00d22cb60a41418073046afb481
kareem_722@hotmail.com <49558122+TheOther-Guy@users.noreply.github.com>
2021-11-24 11:26:53 +0200

    ,

--- python_selenium.py
+++ python_selenium.py
@@ -4,15 +4,11 @@ import selenium
 import time
 os.environ['PATH'] += r"D:/ALL_data_science/Freecodecamp/Selenium-Scraping-Bot" # this slash should be forward slash else it won't work
 driver = webdriver.Chrome()
-driver.get('https://pixishoes.com/')
+driver.implicitly_wait(10)
+driver.get('https://www.crunchbase.com/discover/organization.companies')
 
-time.sleep(10)
-closing_ad = driver.find_element_by_css_selector('#shopify-section-16066484874a6d6f11 > div > div.grid.grid--uniform > div:nth-child(1)').click()
 
-'''time.sleep(20)
-closing_ad.click()'''
-
-'''shoes_category = driver.find_element_by_class_name("grid__item small--one-half medium-up--one-fifth")
-shoes_category.click()'''
+company_card = driver.find_element_by_class_name('identifier-label')
+company_card.click()
 
 
